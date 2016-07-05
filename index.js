2b4e818a0c4f9116c87f93370b6b4d068e297198
Mezo Istvan <istvan.mez@ge.com>
2016-07-05 12:39:12 +0200

    initial contactGroup interface

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
-angular.module("contactGroupServiceModule", [])
+angular.module("contactGroupServiceModule", ["authServiceModule"])
 
-.factory("contactGroupService", function () {
-    return {
+.factory("contactGroupService", function (authService, $q) {
 
-    }
+	var fac = {};
+	
+	fac.write2Console = function() {
+		console.log("contactGroupService is up");
+	};
+
+	fac.listGroups = function() {
+		var groupsList = [
+			{
+				"name":"name1",
+				"displayName":"displayName1"
+			}, {
+				"name":"name2",
+				"displayName":"displayName2"
+			}, {
+				"name":"name3",
+				"displayName":"displayName3"
+			}];
+		var deferred = $q.defer();
+		deferred.resolve(groupsList);
+		
+		return deferred.promise;
+	};
+
+	fac.createGroup = function(name, displayName) {
+		var group = { 
+			"name":name, 
+			"displayName":displayName 
+		};
+		
+		var deferred = $q.defer();
+		deferred.resolve(group);
+		
+		return deferred.promise;
+	};
+
+	fac.renameGroup = function(oldName, newName, displayName) {
+		var group = { 
+			"name":newName, 
+			"displayName":displayName 
+		};
+		
+		var deferred = $q.defer();
+		deferred.resolve(group);
+		
+		return deferred.promise;
+	};
+
+    return fac;
+
 });
